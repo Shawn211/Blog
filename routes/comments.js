7ab26d1fa877cc95943dada606e97fc3a09d2c2b
Shawn211 <csy291422638@gmail.com>
2018-08-31 10:37:14 +0800

    登出; 登陆页; 登陆;

--- routes/comments.js
+++ routes/comments.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+
 const checkLogin = require('../middlewares/check').checkLogin
 
 router.post('/', checkLogin, function(req, res, next){
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+
 const checkLogin = require('../middlewares/check').checkLogin
 
 router.get('/', function(req, res, next){
